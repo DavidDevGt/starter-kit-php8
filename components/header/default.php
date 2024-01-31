1e8dbfe70e2d9b7b8e141703152aac0bb1d7f7ec
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-31 14:39:13 -0600

    Add meta tags for keywords and description

--- components/header/default.php
+++ components/header/default.php
@@ -2,6 +2,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" href="./src/favicon.png" type="image/x-icon">
+<meta name="keywords" content="Starter Kit, para sistemas PHP y MySQL">
+<meta name="description" content="Starter Kit, para sistemas PHP y MySQL">
 <title>Starter Kit</title>
 <link href="../../src/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
