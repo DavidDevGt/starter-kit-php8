aace713631ed65921da750a5f0f0f3537e75a31a
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-30 23:05:41 -0600

    Added tables for user roles and permissions

--- config.sql
+++ config.sql
@@ -7,7 +7,89 @@ CREATE TABLE IF NOT EXISTS user (
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
+  role_id INT NOT NULL,
   active BOOLEAN DEFAULT TRUE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE employee_data (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  name VARCHAR(255),
+  last_name VARCHAR(255),
+  phone VARCHAR(255),
+  address TEXT,
+  active BOOLEAN DEFAULT TRUE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(5) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    description TEXT,
+    active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Config initial data for admin user and roles
+INSERT INTO role (code, name, description) VALUES ('ADMIN', 'Administrador', 'Descripción del rol de administrador');
+INSERT INTO role (code, name, description) VALUES ('GER', 'Gerente', 'Descripción del rol de gerente');
+INSERT INTO role (code, name, description) VALUES ('OFI', 'Oficina', 'Descripción del rol de oficina');
+INSERT INTO role (code, name, description) VALUES ('BOD', 'Bodega', 'Desceripción del rol de bodega');
+INSERT INTO role (code, name, description) VALUES ('VEN', 'Vendedor', 'Descripción del rol de vendedor');
+
+INSERT INTO user (username, password, email, role_id) VALUES ('admin', 'admin', 'joshuexd1@gmail.com', 1);
+
+CREATE TABLE modules (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order INT NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    primary_module BOOLEAN NOT NULL, -- TRUE: primary, FALSE: secondary
+    father_module_id INT NOT NULL DEFAULT 0, -- 0: primary module, "father_module_id": secondary module
+    active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE permission (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    module_id INT NOT NULL,
+    active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    last_user_id INT NOT NULL DEFAULT 0 -- 0: system, "last_user_id": $user_id
+);
+
+CREATE TABLE tax (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    company_id INT NOT NULL,
+    code VARCHAR(10) NOT NULL UNIQUE,
+    name VARCHAR(45) NOT NULL,
+    value DECIMAL(4, 2) NOT NULL,
+    active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE company (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(10) NOT NULL UNIQUE,
+    company_name VARCHAR(255) NOT NULL UNIQUE,
+    company_nit VARCHAR(20) NOT NULL UNIQUE,
+    company_address TEXT,
+    company_postal_code VARCHAR(5),
+    company_phone1 VARCHAR(45),
+    company_phone2 VARCHAR(45),
+    company_phone3 VARCHAR(45),
+    company_email1 VARCHAR(100),
+    company_email2 VARCHAR(100),
+    company_website VARCHAR(150),
+    active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
