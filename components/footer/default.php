518bee01c9b660b933cdb9ee32d1832ba6fa4579
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-31 14:29:26 -0600

    Add footer and header components

---
+++ components/footer/default.php
@@ -0,0 +1,5 @@
+<script src="../../src/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="../../src/assets/jquery/jquery-3.7.1.min.js"></script> -->
+<script src="../../src/assets/alpine/alpine.min.js"></script>
+<script src="../../src/assets/sweetalert/sweetalert2@11.js"></script>
+<script src="../../src/assets/moment/moment.min.js"></script>
---
+++ components/header/default.php
@@ -0,0 +1,13 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="./src/favicon.png" type="image/x-icon">
+<title>Starter Kit</title>
+<link href="../../src/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="../../src/assets/animate/animate.min.css">
+<style>
+    * {
+        font-family: Poppins-Regular, sans-serif;
+    }
+</style>
