4cf282f3c0d0ff680fc8b66c8ce600bec0389931
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-30 23:50:51 -0600

    Add Departament and Municipality models

---
+++ app/models/Departament.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use App\Config\Database;
+
+class Departament extends Model
+{
+    protected $table = 'department';
+
+    public $id;
+}
---
+++ app/models/Municipality.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use App\Config\Database;
+
+class Municipality extends Model
+{
+    protected $table = 'municipality';
+
+    public $id;
+}
