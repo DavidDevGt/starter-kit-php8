b2eba4cdb9072a043347a9bc00b1b5cb0070f337
DavidDevGt <josuedavidvl18@gmail.com>
2024-02-02 14:06:34 -0600

    cambiar de lugar la database class

--- app/config/Database.php
+++ app/config/Database.php
@@ -1,9 +1,9 @@
 <?php
 namespace App\Config;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
 $dotenv->load();
 
 // Imprimir todas las variables de entorno
