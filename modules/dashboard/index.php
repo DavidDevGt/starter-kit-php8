129387ec8eb22fc3c913521905a44ea85042b972
DavidDevGt <josuedavidvl18@gmail.com>
2024-02-02 15:15:40 -0600

    enrutamiento con apache terminado

---
+++ modules/dashboard/index.php
@@ -0,0 +1,20 @@
+<?php
+// Incluir el autoload para cargar las clases
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+// Puedes agregar lógica del controlador aquí o incluir un archivo que la contenga
+
+// Incluir header
+require_once __DIR__ . '/../../components/header/default.php';
+?>
+
+<!-- Aquí va el contenido específico del módulo -->
+<div class="fluid-container">
+    <!-- Contenido ... -->
+    <h1 class="text-center mt-5">Hola</h1>
+</div>
+
+<?php
+// Incluir footer
+require_once __DIR__ . '/../../components/footer/default.php';
+?>
