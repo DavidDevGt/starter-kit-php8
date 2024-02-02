73918bb779f1c8ea61461665bdfdc17107339188
DavidDevGt <josuedavidvl18@gmail.com>
2024-02-02 16:19:24 -0600

    menu component added without the logic of bbdd

---
+++ components/menu/component.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-primary p-3 shadow-lg w-full" data-bs-theme="dark">
+    <div class="container-fluid">
+        <img height="35" class="ms-4 me-4" src="../../src/assets/images/logo-starter-kit.png">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="bi bi-list fs-5 text-white"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="navbar_area">
+            </ul>
+            <div role="search">
+                <button class="btn btn-danger" type="button">Cerrar Sesión</button>
+            </div>
+        </div>
+    </div>
+</nav>
